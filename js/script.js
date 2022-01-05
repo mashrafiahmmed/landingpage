3b784c55bfba89e1cf4bd28157713e32777ea2ef
mashrafiahmmed <97114753+mashrafiahmmed@users.noreply.github.com>
2022-01-05 15:24:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.timeLine').timeLine({
+        mainColor: '#4DB7AA',
+        opacity: '0.85',
+        lineColor: '#fafafa'
+    });
+
+    
+$(function() {
+    var $gallery = new SimpleLightbox('.gallery a', {});
+})();
